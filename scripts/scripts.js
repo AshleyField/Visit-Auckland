e7a0857a4e53a26b1e904e5ea72f15ee8a05c88a
Ashley Field <ashleyfield@Ashleys-MacBook-Pro.local>
2018-09-05 15:17:29 +1200

    Started hero and nav area

---
+++ scripts/scripts.js
@@ -0,0 +1,25 @@
+$(function() {
+
+    // Open and close nav on mobile
+    $('.bars').on('click', function(e) {
+
+        var navData = $('.navigation').data('nav');
+
+        e.stopPropagation();
+
+        if (navData == 'close') {
+            $('.navigation').addClass('nav-open')
+                .data('nav', 'open')
+
+            $('.bars>i').first().removeClass('fas fa-bars')
+                .addClass('fas fa-times');
+
+        } else {
+            $('.navigation').removeClass('nav-open')
+                .data('nav', 'close');
+            $('.bars>i').removeClass('fas fa-times')
+                .addClass('fas fa-bars');
+
+        }
+    });
+});
