9e49f3e056a9659e801b28066c162b3159bce377
Ashley Field <ashleyfield@Ashleys-MacBook-Pro.local>
2018-09-11 14:54:51 +1200

    Implemented Leaflet and added custom map styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,7 @@
 $(function() {
 
     // Open and close nav on mobile
+
     $('.bars').on('click', function(e) {
 
         var navData = $('.navigation').data('nav');
@@ -24,7 +25,7 @@ $(function() {
         }
     });
 
-
+    //Prevent login button from refreshing the page.
     
     $('.login-button').on('click', function(e){
         e.preventDefault();
@@ -43,13 +44,90 @@ $(function() {
 	  }
 	});
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoibWx5MDIyMyIsImEiOiJjamxuNGc5Zm8xZjdyM2twaGpsa3E2cnRmIn0.YC9jrYJYIQWj25EF4Wn3Zg';
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mly0223/cjln4h5eu5wca2snqpippvx3f',
-        center: [174.767164, -36.851146],
-        zoom: 15.3
-    });
+    //Initiate leaflet key
+
+    const version = '?v=20170901';
+    const clientid = '&client_id=1C3ZIZUJZDFCWZPWN1NS4F3RUBALGDNH5HUFLVSNKISZOABA';
+    const clientSecret = '&client_secret=11SXOUY3WXTWN1HJJ00NIU22UCV2WRLW33DOLZWUNQVKLIHK';
+    const key = version + clientid + clientSecret;
+
+
+    //Insert mapbox styles
+
+    var center = {lat: -36.842770,lng: 174.766930};
+
+    var map = L.map('map', {scrollWheelZoom: false}).setView(center,16);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXNoZXlmaWVsZCIsImEiOiJjamtrZXlvNnMwZTg3M3FwYzAxbGNqYTA4In0.AyMC7APOvh72_Q2evO5VTQ').addTo(map);
+
+    L.circle(center, {
+        radius: 200,
+        color: '#000',
+        weight: 2,
+        fillColor: '#000',
+        fillOpacity: .1
+    }).addTo(map);
+
+    //Initalise foursquare
+
+    let exploreUrl = 'https://api.foursquare.com/v2/venues/explore'+ key +'&ll=-36.8446152873055,174.76662397384644'
+
+    $.ajax({
+        url: exploreUrl,
+        dataType: 'jsonp',
+        success: function(res){
+
+            var data = res.response.groups["0"].items;
+
+            var venues = _(data).map(function(item){
+                return {
+                    latlng: {lat: item.venue.location.lat, lng: item.venue.location.lng},
+                    name: item.venue.name,
+                    venueid: item.venue.id,
+                    category: item.venue.categories['0'].name
+                }
+            });
+
+            console.log(data);
+            console.log(venues);
+
+            _(venues).each(function(venue){
+                console.log('hi');
+
+                let venueIcon = L.icon({
+                    iconUrl: 'assets/cafe.svg',
+                    iconSize: [30,30]
+                });
+
+                let marker = L.marker(venue.latlng, {icon: venueIcon}).addTo(map);
+                // marker.venueid = venue.venueid
+
+                // marker.on('click', function(){
+
+                //     var venueUrl = 'https://api.foursquare.com/v2/venues/'+ this.venueid + key;
+
+                //     $.ajax({
+                //         url: venueUrl,
+                //         dataType: 'jsonp',s
+                //         success: function(res){
+
+                //             console.log(res);
+                //             var venue = res.response.venue;
+                //             $('.modal-title').empty();
+                //             $('.modal-title').text(venue.name);
+                //             var photo = venue.bestPhoto;
+                //             var photoSource = photo.prefix+ '100x100' +photo.suffix;
+                //             $('.modal-body').empty();
+                //             $('<img src="'+ photoSource +'">').appendTo('.modal-body');
+                //         }
+                //     })
+
+                //     $('#venueModal').modal('show');
+
+                // });
+            })
+        }
+    })
 
 });
 
